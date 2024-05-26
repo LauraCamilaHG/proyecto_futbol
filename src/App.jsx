8ba0f0d4bf145ce7b836bbfa0ca55fed1f4fa19d
LauraCamilaHG <camihernandezg@gmail.com>
2024-05-26 16:30:02 -0500

    Primer paso proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './styles.css'; // Asegúrate de tener tus estilos en este archivo o ajusta la ruta según sea necesario
 
-// Mateo Commit
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-//prueba cambios camila
-export default App
-
-// sebastian comentario
-// prueba cambio mateo 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [highlightEvenRows, setHighlightEvenRows] = useState(false);
+  const [highlightOddRows, setHighlightOddRows] = useState(false);
+  const [range, setRange] = useState([0, 20]); // Estado para controlar el rango de jugadores a mostrar
